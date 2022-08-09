9ff43fcc7df16f77bf2c04d183a65ec7b3f47594
Moyses Azevedo <m.mirazev@gmail.com>
2022-08-09 11:00:32 -0300

    Initial Commit

---
+++ Skynet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Skynet
+//
+//  Created by Moyses Miranda do Vale Azevedo on 09/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
